6037670a724d1243e4ac3f615de3bd7e3b156f63
Hugo Firth <hugo.firth@gmail.com>
2016-11-28 18:53:08 +0000

    initial kind of working version need to fix centring the london data

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to osm-map
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/osm_map/core_test.clj
@@ -0,0 +1,7 @@
+(ns osm-map.core-test
+  (:require [clojure.test :refer :all]
+            [osm-map.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
